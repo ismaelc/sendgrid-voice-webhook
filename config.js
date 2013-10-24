20938008ba0b049c520e9b8eca0e51b99ee49784
Chris Ismael <chris.ismael@gmail.com>
2013-10-24 16:48:34 -0700

    removed config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.mashape = {};
-config.mashape.key = "7uo4fXqGd3DyQVMHNCxrnmgPWnHqfHAZ";
-
-config.firebase = {};
-config.firebase.hostname = "https://voice.firebaseio.com/";
-
-module.exports = config;
