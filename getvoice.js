5f775fa8f95684e69a138377fea09334cee72e6a
Chris Ismael <chris.ismael@gmail.com>
2013-10-24 16:25:56 -0700

    Optimized code

--- getvoice.js
+++ getvoice.js
@@ -8,8 +8,7 @@ var myRootRef = new Firebase(config.firebase.hostname);
 // Method POSTed to by Sendgrid's Inbound Parse API (Webhooks) - http://bit.ly/11h7K2P
 // 1.  Accepts 'subject' and 'text' 
 // 2.  Calls a Mashape text-to-speech API on 'subject'
-// 3.  Saves text-to-speech output as mp3
-// 4.  Pushes info to Firebase to initiate hook to play mp3 from index.html
+// 3.  Pushes resulting mp3 to Firebase to initiate hook to play mp3 from index.html
  
 exports.voice = function (req, res) {
 
@@ -22,15 +21,6 @@ exports.voice = function (req, res) {
      })
      .encoding('binary')
      .end(function (response) {
-        fs.writeFile('public/response.mp3', response.response.body, {"encoding":"binary"}, function(err) {
-           if(err) {
-              console.log(err);
-              res.send({"error":err});
-           } else {
-              console.log("MP3 file for '" + subj + "' created!");
-              res.send({"played":subj});
-           }
-        });
         myRootRef.push({subj: subj, text: text, voice: response.response.body});
      });
 }
