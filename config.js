2ef19e6562a2cfd309a7e289b23670a48faa2070
Chris Ismael <chris.ismael@gmail.com>
2013-10-24 17:14:34 -0700

    added config for heroku

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.mashape = {};
+config.mashape.key = "7uo4fXqGd3DyQVMHNCxrnmgPWnHqfHAZ";
+
+config.firebase = {};
+config.firebase.hostname = "https://voice.firebaseio.com/";
+
+module.exports = config;
