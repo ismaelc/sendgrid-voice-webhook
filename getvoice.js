4c19334d89a7191e01eec41710a6cfa4ea5cc834
Chris Ismael <chris.ismael@gmail.com>
2013-10-24 17:07:06 -0700

    Added response

--- getvoice.js
+++ getvoice.js
@@ -5,7 +5,7 @@ var unirest = require('unirest'),
 
 var myRootRef = new Firebase(config.firebase.hostname);
 
-// Method POSTed to by Sendgrid's Inbound Parse API (Webhooks) - http://bit.ly/11h7K2P
+// Method below is POSTed to by Sendgrid's Inbound Parse API (Webhooks) - http://bit.ly/11h7K2P
 // 1.  Accepts 'subject' and 'text' 
 // 2.  Calls a Mashape text-to-speech API on 'subject'
 // 3.  Pushes resulting mp3 to Firebase to initiate hook to play mp3 from index.html
@@ -16,11 +16,10 @@ exports.voice = function (req, res) {
    var text = req.body.text;
   
    var request = unirest.get("https://montanaflynn-text-to-speech.p.mashape.com/speak?text=" + subj)
-     .headers({ 
-       "X-Mashape-Authorization": config.mashape.key,
-     })
+     .set("X-Mashape-Authorization", config.mashape.key)
      .encoding('binary')
      .end(function (response) {
         myRootRef.push({subj: subj, text: text, voice: response.response.body});
+        res.send({"played":subj});
      });
 }
